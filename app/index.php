9b96cba5ad5f2cf0e31abce43ac1eda7e25f1a2f
Vitor <vitorgpalves@gmail.com>
2019-08-05 00:29:45 -0300

    feature(09): finishes the class Prepare Stmt

--- app/index.php
+++ app/index.php
@@ -3,15 +3,17 @@
 require_once("connection.php");
 
 $allProductsQuery = "select * from products;";
-$singularProductQuery = "select * from products where id = {$_GET['id']};";
+$singularProductQuery = "select * from products where id = :id;";
 
 $allProductsStatement = $connection->query($allProductsQuery);
-$singularProductStatement = $connection->query($singularProductQuery);
+$singularProductStatement = $connection->prepare($singularProductQuery);
+$singularProductStatement->bindValue(":id", $_GET['id']);
+$singularProductStatement->execute();
 
 $singularProduct = $singularProductStatement->fetch(PDO::FETCH_OBJ);
 echo $singularProduct->name . "<br>";
 echo $singularProduct->description;
-echo "=======================================";
+echo "<br>=======================================<br>";
 foreach ($allProductsStatement as $product) {
   echo $product["name"] . "<br>";
 }
