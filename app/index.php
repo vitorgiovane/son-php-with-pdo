d4b351bf97fdb5ea518be88f8623efc7ccbda621
Vitor <vitorgpalves@gmail.com>
2019-08-04 23:30:13 -0300

    feature(06): finishes the class Working with Fetch

--- app/index.php
+++ app/index.php
@@ -3,6 +3,12 @@
 require_once("connection.php");
 
 $allProductsQuery = "select * from products;";
-$allProductsStatement = $connection->query($allProductsQuery);
+$singularProductQuery = "select * from products where id = 2;";
 
-var_dump($allProductsStatement->fetchAll(PDO::FETCH_OBJ));
+$allProductsStatement = $connection->query($allProductsQuery);
+$singularProductStatement = $connection->query($singularProductQuery);
+
+echo "<pre>";
+$singularProduct = $singularProductStatement->fetch(PDO::FETCH_OBJ);
+echo $singularProduct->name . "<br>";
+echo $singularProduct->description;
